ee769fc3a175f517e405b954c65c131a19590f33
Sawsan Elsebaey <sawsanelsebay2@gmail.com>
2025-01-25 20:23:07 +0200

    Solve C# Task1 & Task2

---
+++ Team-Tasks/Sawsan/Program.cs
@@ -0,0 +1,13 @@
+﻿using Sawsan.Tasks;
+
+namespace Sawsan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // C_Task1.RunTasks();
+           // C_Task2.RunTasks();
+        }
+    }
+}
